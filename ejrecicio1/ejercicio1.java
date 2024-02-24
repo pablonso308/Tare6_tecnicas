6bbfa0b2f7fb802b6aa83b9b4045803187515d91
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-24 20:45:05 +0100

    ejercicio1

---
+++ ejrecicio1/ejercicio1.java
@@ -0,0 +1,35 @@
+package ejrecicio1;
+
+
+public class ejercicio1 {
+
+
+
+    public static void mayorDeDos(int numero1, int numero2) {
+        if (numero1 > numero2) {
+            System.out.println("El mayor es: " + numero1);
+        } else {
+            System.out.println("El mayor es: " + numero2);
+        }
+    }
+
+    public static void clasificarNumeros(int numero1, int numero2) {
+        if (numero1 == numero2) {
+            System.out.println("La clasificación es: 0");
+        } else if (numero1 > numero2) {
+            System.out.println("La clasificación es: 1");
+        } else {
+            System.out.println("La clasificación es: -1");
+        }
+    }
+
+
+
+}
+
+
+//número1 = obtenerNúmero("Ingrese el primer número: ")
+//número2 = obtenerNúmero("Ingrese el segundo número: ")
+//mayor = mayorDeDos(número1, número2)
+//clasificación = clasificarNúmeros(número1, número2)
+//imprimir("El mayor es: ", mayor, " y la clasificación es: ", clasificación)
---
+++ ejrecicio1/main.java
@@ -0,0 +1,17 @@
+package ejrecicio1;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero1, numero2;
+        System.out.println("Ingrese el primer número: ");
+        numero1 = sc.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        numero2 = sc.nextInt();
+        ejercicio1.mayorDeDos(numero1, numero2);
+        ejercicio1.clasificarNumeros(numero1, numero2);
+
+    }
+}
